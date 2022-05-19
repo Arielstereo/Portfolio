f9f890b66d71e29ce0179236cfd8cee99c99b3bf
Ariel Martinez <arielstereo@msn.com>
2022-05-19 20:31:17 -0300

    add dist folder

---
+++ dist/_nuxt/a2fa2ff.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{270:function(t,e,n){"use strict";n.r(e);var l=n(17),component=Object(l.a)({},(function(){var t=this,e=t.$createElement;return(t._self._c||e)("h2",{staticClass:"text-4xl md:text-5xl dark:text-white animate__animated animate__zoomInDown"},[t._t("default")],2)}),[],!1,null,null,null);e.default=component.exports}}]);
